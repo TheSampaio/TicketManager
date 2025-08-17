a3ed49e085f9a84fbea018862821d4f2a3beee15
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-17 09:50:00 -0300

    Update AppDbContext.cs
    - Add DbSet<TicketModel> into the class.

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<TicketModel> Tickets { get; set; }
     }
 }
